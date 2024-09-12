18938f82ce0bb5d6a34dd8bd32d971521e1717be
Gemi <166994392+aquahey@users.noreply.github.com>
2024-09-12 20:16:43 +0800

    Update search.blade.php

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -17,7 +17,7 @@
             </div>
 
             <!-- search bar section-->
-            <div class="flex justify-between items-center mb-4">
+            <div class="absolute top-24 left-6">
                 <div class="relative w-64">
                     <i class="fa fa-search absolute left-3 top-1/2 transform -translate-y-1/2 text-black"></i>
                     <input type="text" placeholder="Search" class="w-full h-10 pl-10 pr-4 rounded-lg bg-white shadow border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-700">
