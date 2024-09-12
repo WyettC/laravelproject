5daba95df4816ddb032b90d55866978cbc48b437
Gemi <166994392+aquahey@users.noreply.github.com>
2024-09-12 20:16:20 +0800

    Update pagination.blade.php

--- resources/views/pagination.blade.php
+++ resources/views/pagination.blade.php
@@ -9,25 +9,26 @@
     </head>
     
     <body class="min-h-screen bg-[#C8D9E2]">
-        <!-- Main Container -->
+    <!-- Main Container -->
         <div class="container mx-auto px-6 py-10">
             <!-- pagination section -->
-            <div class="w-full py-4 px-8 flex md:flex-row justify-between items-center absolute bottom-10 left-0">
-            <!-- previous button -->
-            <div class="w-24 md:w-auto mb-2 md:mb-0 text-center md:text-left">
-                <button class="bg-white px-4 py-2 border border-gray-300 font-semibold text-black rounded-lg">
-                    Previous
-                </button>
-            </div>
-            <!-- page indicator -->
-            <div class="w-full md:w-auto mb-2 md:mb-0 font-semibold text-center text-black">
-                <span>Page 1 of 10</span>
-            </div>
-            <!-- next button -->
-            <div class="w-24 md:w-auto text-center md:text-right">
-                <button class="bg-white px-4 py-2 border border-gray-300 font-semibold text-black rounded-lg">
-                    Next
-                </button>
+            <div class="w-full py-4 px-6 flex justify-center sm:justify-between items-center absolute bottom-10 left-0 right-0">
+                <!-- previous button -->
+                <div class="w-auto text-left">
+                    <button class="w-24 bg-white px-4 py-2 border border-gray-300 font-semibold text-black rounded-lg hover:bg-[#2E5090] hover:text-white">
+                        Previous
+                    </button>
+                </div>
+                <!-- page indicator -->
+                <div class="w-auto text-center font-normal sm:font-semibold text-black text-base sm:text-lg whitespace-nowrap mx-4">
+                    <span>Page 1 of 10</span>
+                </div>
+                <!-- next button -->
+                <div class="w-auto text-right">
+                    <button class="w-24 bg-white px-4 py-2 border border-gray-300 font-semibold text-black rounded-lg hover:bg-[#2E5090] hover:text-white">
+                        Next
+                    </button>
+                </div>
             </div>
         </div>
     </body>
