25b50fc11688a13139a5f7f78dd0e1f99e4b0044
Gemi <166994392+aquahey@users.noreply.github.com>
2024-09-18 21:24:25 +0800

    Update pagination.blade.php

--- resources/views/pagination.blade.php
+++ resources/views/pagination.blade.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script src="https://cdn.tailwindcss.com"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <title>Attendance</title>
-    </head>
-    
-    <body class="min-h-screen bg-[#C8D9E2]">
+@extends('layouts.layout')
+
+@section('title', 'Pagination Buttons')
+
+@section('content')
     <!-- Main Container -->
         <div class="container mx-auto px-6 py-10">
             <!-- pagination section -->
@@ -31,5 +25,3 @@
                 </div>
             </div>
         </div>
-    </body>
-</html>
